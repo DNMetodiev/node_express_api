030a79f5d6cf5a9fef34bfdd0ab37b75ffb09317
DNMetodiev <91205942+DNMetodiev@users.noreply.github.com>
2023-10-03 01:45:47 +0300

    Make requests with dummy data

--- app.js
+++ app.js
@@ -1,16 +1,85 @@
 import express from 'express';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
 
-  res.status(200).json({ message: 'Hello from the server', app: "Tours" });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+// app.get('/', (req, res) => {
+
+//   res.status(200).json({ message: 'Hello from the server', app: "Tours" });
+
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can POST');
+// })
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`
+
+  ));
+
+//req + res = route handler
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  })
 });
 
-app.post('/', (req, res) => {
-  res.send('You can POST');
-})
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "invalid ID"
+    })
+  }
+
+  const tour = tours.find(el => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+
+
+    data: {
+      tour
+    }
+  })
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  });
+
+
+});
 
 const port = 3000;
 app.listen(port, () => {
